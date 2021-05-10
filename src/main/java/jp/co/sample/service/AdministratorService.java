abb20e5ca63248d447421a543f522f8510e8ebc4
Kaori Abe <kaori.abe@rakus-partners.co.jp>
2021-05-10 14:18:42 +0900

    AdministratorServiceクラスにinsertメソッドを定義

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -3,6 +3,8 @@ package jp.co.sample.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 /** 
@@ -16,4 +18,10 @@ public class AdministratorService {
 
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/** 管理者情報を挿入するメソッド */
+	public void insert(Administrator administrator) {
+		//administratorRepositoryのinsert()メソッドを呼ぶ
+		administratorRepository.insert(administrator);
+	}
 }
